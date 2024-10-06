2f52d2df61298485dac9b97c891d35d2506d3217
Nicholas Ciarlone <nicholasciarlone@gmail.com>
2024-10-06 15:05:43 -0400

    Icon objects + scripting

--- Assets/Scripts/UILogic.cs
+++ Assets/Scripts/UILogic.cs
@@ -7,10 +7,30 @@ using UnityEngine;
 
 public class UILogic : MonoBehaviour
 {
+    public static int currentLevel = 1;
     public bool paused = false;
     public GameObject PauseMenu;
     public GameObject PauseButton;
 
+    //level 1 icons
+    public GameObject lungsIcon;
+    public GameObject wigIcon;
+    public GameObject intestinesIcon;
+
+    //level 2 icons
+    public GameObject teethIcon;
+    public GameObject appendixIcon;
+    public GameObject brainIcon;
+
+    //scan icons
+    public GameObject honkRateIcon;
+    public GameObject waterPressureIcon;
+    public GameObject melancholyIcon;
+
+    public void Update()
+    {
+        LevelChange();
+    }
 
     public void StartGame()
     {
@@ -19,10 +39,7 @@ public class UILogic : MonoBehaviour
 
     public void StartLevel()
     {
-        if (SceneManager.GetActiveScene().name == "Intro")
-        {
-            SceneManager.LoadSceneAsync("Level");
-        }
+        SceneManager.LoadSceneAsync("Level");
     }
 
     public void Settings()
@@ -61,7 +78,7 @@ public class UILogic : MonoBehaviour
 
     public void LinkToManual()
     {
-        Application.OpenURL("https://youtu.be/dQw4w9WgXcQ?feature=shared");
+        Application.OpenURL("https://docs.google.com/document/d/1YWzMZ1MN0obeqviIon48yFX1O2DJqQxLSIFmgHyzrfQ/edit?usp=sharing");
     }
 
     public void QuitToDesktop()
@@ -76,14 +93,13 @@ public class UILogic : MonoBehaviour
 
     public void Pause()
     {
-        if (!paused)
+        if(!paused)
         {
             paused = true;
             Time.timeScale = 0;
             PauseMenu.SetActive(true);
             PauseButton.SetActive(false);
             Debug.Log(paused);
-
         }
         else if(paused)
         {
@@ -93,6 +109,65 @@ public class UILogic : MonoBehaviour
             PauseButton.SetActive(true);
             Debug.Log(paused);
         }
+    }
+
+    public void LevelChange()
+    {
+        if(SceneManager.GetActiveScene().name == "Level" && currentLevel == 1)
+        {
+            Debug.Log("level 1");
+            Debug.Log(currentLevel);
+
+            //in use
+            lungsIcon.SetActive(true);
+            wigIcon.SetActive(true);
+            intestinesIcon.SetActive(true);
+            honkRateIcon.SetActive(true);
+            waterPressureIcon.SetActive(true);
+
+            //not in use 
+            teethIcon.SetActive(false);
+            brainIcon.SetActive(false);
+            appendixIcon.SetActive(false);
+            melancholyIcon.SetActive(false);
+
+        }
+        else if(SceneManager.GetActiveScene().name == "Level" && currentLevel == 2)
+        {
+            Debug.Log("level 2");
+
+            //in use 
+            teethIcon.SetActive(true);
+            brainIcon.SetActive(true);
+            appendixIcon.SetActive(true);
+            melancholyIcon.SetActive(true);
+            honkRateIcon.SetActive(true);
+
+            //not in use
+            waterPressureIcon.SetActive(false);
+            lungsIcon.SetActive(false);
+            wigIcon.SetActive(false);
+            intestinesIcon.SetActive(false);
+
+        }
+        else if(SceneManager.GetActiveScene().name == "Level" && currentLevel == 3)
+        {
+            Debug.Log("level 3");
+            //remove appendix
+            appendixIcon.SetActive(false);
+
+            //in use
+            lungsIcon.SetActive(true);
+            wigIcon.SetActive(true);
+            intestinesIcon.SetActive(true);
+            teethIcon.SetActive(true);
+            brainIcon.SetActive(true);
+            waterPressureIcon.SetActive(true);
+            honkRateIcon.SetActive(true);
+            melancholyIcon.SetActive(true);
+
+        }
+        Debug.Log("end");
     }
 
     
